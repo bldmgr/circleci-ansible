5cf194fd60a13191a7a6757bc32497328e312270
Mike Montpetit <mimontpe@cisco.com>
2024-10-22 15:53:02 -0400

    aws testing

--- roles/windows/install_cloudtools/files/install_servicefabric_sdk.ps1
+++ roles/windows/install_cloudtools/files/install_servicefabric_sdk.ps1
@@ -1,6 +1,4 @@
 #Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
-Get-ExecutionPolicy -List
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 $servicefabricsdk_version = $args[0]
 choco install service-fabric-sdk --version $servicefabricsdk_version -y
